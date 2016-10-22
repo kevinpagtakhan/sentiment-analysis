223bba8a54fbb60e9a46bf32291026a2790e1f5c
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-10-22 15:34:27 -0700

    Connect to database and use post route

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ var app = express();
 var logger = require('morgan');
 var dotenv = require('dotenv').load({silent: true});
 var AYLIENTextAPI = require('aylien_textapi');
+var mongoose = require('mongoose');
 var textapi = new AYLIENTextAPI({
   application_id: process.env.TA_APP_ID,
   application_key: process.env.TA_KEY
 });
 
+var postRoutes = require('./routes/api/posts');
+
+mongoose.connect('mongodb://localhost/postanalysis', function(err, db){
+  console.log(err || 'Connected to database');
+})
+
 app.use(logger('dev'));
 app.use(express.static('public'));
+
+app.use('/api/posts', postRoutes);
 
 app.post('/api/sentiment', function(req, res){
   textapi.sentiment({
